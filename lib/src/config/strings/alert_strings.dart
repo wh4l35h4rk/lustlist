22fff8ddcb41d517bbf397ec40eb194b0dce7ece
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-11-26 17:45:21 +1000

    (fix) alert strings

--- lib/src/config/strings/alert_strings.dart
+++ lib/src/config/strings/alert_strings.dart
@@ -2,12 +2,12 @@ class AlertStrings {
   static const String deleteEvent = "Are you sure you want to delete this event? This action can't be undone.";
   static const String editEvent = "Are you sure you want to leave this menu? Your event won't be saved.";
 
-  static const String deletePartner = "Are you sure you want to delete this partner? All events with this partner will be deleted, too. This action can't be undone.";
+  static const String deletePartner = "Are you sure you want to delete this partner? This action can't be undone.";
   static const String noDeletePartner = "You can't delete this partner if you have events with them!";
   static const String editPartner = "Are you sure you want to leave this menu? Your partner profile won't be saved.";
 
   static const String noEventPartner = "You need to select at least one partner.";
-  static const String noteTooLong = 'Your note extends maximal length!';
+  static const String noteTooLong = "Your note extends maximal length!";
   static const String noName = "Please enter name";
   static const String nameTooLong = 'Your name extends maximal length!';
   static const String noPartnersPassed = "No partners for this event.";
